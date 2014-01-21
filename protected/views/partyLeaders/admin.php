4ecbf3401fd7b305ef961777ded2031659eb4ab3
Michalis Katsarakis <mkats@ics.forth.gr>
2014-01-21 19:18:51 +0200

    Related model information on CRUD ops on party_leaders

--- protected/views/partyLeaders/admin.php
+++ protected/views/partyLeaders/admin.php
@@ -47,6 +47,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 	'columns'=>array(
 		'party_leader_id',
 		'person_id',
+		"person.name",
 		array(
 			'class'=>'CButtonColumn',
 		),
