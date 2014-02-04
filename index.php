6571c13d128392eebca15d563e3f287185de02d5
Michalis Katsarakis <mkats@ics.forth.gr>
2014-02-04 23:23:54 +0200

    Changing Yii framework lib directory location.

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii-1.1.14.f0fee9/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+$yii=dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'yii-1.1.14.f0fee9'.DIRECTORY_SEPARATOR.'framework'.DIRECTORY_SEPARATOR.'yii.php';
+$config=dirname(__FILE__).DIRECTORY_SEPARATOR.'protected'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'main.php';
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
